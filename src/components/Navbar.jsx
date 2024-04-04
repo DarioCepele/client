08662a549db568a2ce77b4ba0726db11fc80562a
Dario Cepele <64326626+DarioCepele@users.noreply.github.com>
2024-04-04 09:06:00 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             Home
           </Link>
           <Link to={"/favorites"} className="mr-8 text-gray-950">
-            MyRecipe
+            MyRecipes
           </Link>
           <Link to={"/contact"} className="mr-8 text-gray-950">
             Contact
@@ -72,7 +72,7 @@ const Navbar = () => {
             Home
           </Link>
           <Link to="/favorites" className="block px-3 py-2 rounded-md text-gray-950  hover:bg-indigo-700">
-            MyRecipe
+            MyRecipes
           </Link>
           <Link to="/contact" className="block px-3 py-2 rounded-md text-gray-950  hover:bg-indigo-700">
             Contact
